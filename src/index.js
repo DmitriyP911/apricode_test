a94fe71dff0986c3c48206f3287931399dbd053b
Dmitriy Pismenniy <dima30655@gmail.com>
2020-09-07 23:15:58 +0300

    test task for apricode company

--- src/index.js
+++ src/index.js
@@ -232,22 +232,22 @@ var slider = multiItemSlider( '.slider', {
     isCycling: true
 } )
 
-// const input = document.querySelector( '.footer-input' );
-// const inputButton = document.querySelector( '.button' );
+const input = document.querySelector( '.footer-input' );
+const inputButton = document.querySelector( '.button' );
 
-// input.addEventListener( 'change', () => {
-//     if( input.value.length > 6 ) {
-//         inputButton.removeAttribute( "disabled" );
-//     } else inputButton.setAttribute( "disabled", true );
+input.addEventListener( 'change', () => {
+    if( input.value.length > 6 ) {
+        inputButton.removeAttribute( "disabled" );
+    } else inputButton.setAttribute( "disabled", true );
 
-// } )
+} )
 
-// inputButton.addEventListener( 'click', ( e ) => {
-//     e.preventDefault();
-//     const user = {
-//         message: `Подписался пользователь с имэйлом ${input.value}`
-//     }
-//     alert( user.message );
-//     input.value = "";
-//     inputButton.setAttribute( "disabled", true );
-// } )
+inputButton.addEventListener( 'click', ( e ) => {
+    e.preventDefault();
+    const user = {
+        message: `Подписался пользователь с имэйлом ${input.value}`
+    }
+    alert( user.message );
+    input.value = "";
+    inputButton.setAttribute( "disabled", true );
+} )
